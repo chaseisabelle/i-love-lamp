fae7ed14c12d3e9e1d08b87e66bb940c804ef0d4
a dog that can talk <adogthatcantalk@gmail.com>
2019-08-20 23:14:34 +0200

    did something...?

--- php/website.php
+++ php/website.php
@@ -2,5 +2,5 @@
 require_once(realpath(__DIR__ . '/../vendor/autoload.php'));
 
 function get_title() {
-    return utf8_encode(string_jazzify('this is a website'));
+    return implode('', alphabet());
 }
